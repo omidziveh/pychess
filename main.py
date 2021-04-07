8adc78c4a3b69c126df5015a6a1836921edafa81
omidziveh <omidziveh1386@gmail.com>
2021-04-07 11:14:47 +0430

    some problems solved

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
-import pygame
 import sys
+import chess
+import pygame
 
 from data import colors
 from widgets import board
+from widgets import size
 
-pygame.init()
+code_board = chess.Board()
 
-screen = pygame.display.set_mode((600, 400), pygame.RESIZABLE)
+print(code_board.fen())
 
-board = board.Board(screen, (10, 20), 30)
+screen = pygame.display.set_mode((600, 600), pygame.RESIZABLE)
+#
+gui_board = board.Board(screen, size.Size(100, 100, 400, 400), 'w', 50)
+# print(gui_board.generate_fen("8/8/8/8/8/8/8 b KQkq - 0 4"))
+# print(gui_board.generate_fen("r1bqkb1r/pppp1Qpp/2n2n2/4p3/2B1P3/8/PPPP1PPP/RNB1K1NR b KQkq - 0 4"))
+# print(gui_board.generate_fen(code_board.fen()))
+# print(gui_board.pieces)
+# print(code_board)
 
 while True:
     screen.fill(colors.white)
-    
-    board.draw()
-    for ev in pygame.event.get():
-        if ev.type == pygame.QUIT:
+
+    gui_board.draw()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
             sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(gui_board.coord(event.pos))
+            gui_board.draw_piece(gui_board.coord(event.pos))
     pygame.display.update()
+    pygame.time.Clock().tick(60)
--- widgets/board.py
+++ widgets/board.py
@@ -1,35 +1,91 @@
 import pygame
-from pygame.draw import line
 
 from data import colors
 
+
 class Board:
-    def __init__(self, screen, pos, sqr_size, padding=10, theme=(colors.black, colors.white), width=2):
-        self.sqr_size = sqr_size # Integer
-        self.screen = screen # pygame.display
-        self.pos = pos # (Integer X, Integer Y)
-        self.theme = theme # (color dark, color light)
-        self.padding = padding # Integer
-        self.width = width # Integer
+    def __init__(self, screen, table_size, side, sqr_size, padding=100, theme=(colors.brown, colors.light_brown), width=2):
+        self.screen = screen  # pygame.display
+        self.size = table_size  # Size
+        self.side = side  # "colors.white" / "colors.black"
+        self.sqr_size = sqr_size # int
+        self.theme = theme  # (color dark, color light)
+        self.padding = padding  # Integer
+        self.width = width  # Integer
+        self._rect = pygame.Rect(self.size.x, self.size.y, self.size.width, self.size.height)
+        self._pieces = {}
     
     def draw(self):
+        pygame.draw.rect(
+            self.screen,
+            self.theme[0],
+            self.rect
+        )
+
+        for i in range(self.size.left, self.size.right, self.sqr_size * 2):
+            for j in range(self.size.top, self.size.bottom, self.sqr_size * 2):
+                pygame.draw.rect(
+                    self.screen,
+                    self.theme[1],
+                    (i, j, self.sqr_size, self.sqr_size)
+                )
+
+        for i in range(self.size.left + self.sqr_size, self.size.right, self.sqr_size * 2):
+            for j in range(self.size.top + self.sqr_size, self.size.bottom, self.sqr_size * 2):
+                pygame.draw.rect(
+                    self.screen,
+                    self.theme[1],
+                    (i, j, self.sqr_size, self.sqr_size)
+                )
+
+    def coord(self, pos):
+        rect = ((pos[0] - self.size.left) // self.sqr_size, 7 - (pos[1] - self.size.top) // self.sqr_size)
+        return f'{rect[1]+1}{chr(97+rect[0])}'
+
+    # def generate_fen(self, string):
+    #     string = string.split()[0]
+    #     piece_list = string.split('/')
+    #     new_string = []
+    #     print(string)
+    #     for i in range(len(piece_list)):
+    #         new_string.append(split_numbers(piece_list[i]))
+    #     return new_string
+
+    def draw_piece(self, point):
+        print(8-int(point[0]))
+        print(ord(point[1]) - 97)
+        piece_rect = pygame.Rect(
+            (ord(point[1]) - 97) * self.sqr_size + self.size.left,
+            (8-int(point[0])) * self.sqr_size + self.size.top,
+            self.sqr_size,
+            self.sqr_size
+        )
+        pygame.draw.rect(
+            self.screen,
+            colors.green,
+            piece_rect
+        )
+
+
+    @property
+    def rect(self):
+        return self._rect
+
+    @property
+    def pieces(self):
+        return self._pieces
+
+    @rect.setter
+    def rect(self, value):
+        self._rect = value
+
+    @pieces.setter
+    def pieces(self, value):
+        self._pieces = value
+
+
+def convert_fen_piece_to_uri(string):
+    piece_name = ['k', 'q', 'b', 'n', 'r', 'p', 'K', 'Q', 'B', 'N', 'R', 'P']
+    complete_name = ['Wk', 'Wq', 'Wb', 'Wn', 'Wr', 'Wp', 'Bk', 'Bq', 'Bb', 'Bn', 'Br', 'Bp']
+    if string in piece_name:
         pass
-        # draw vertical lines
-        # for i in range(9):
-        #     pygame.draw.line(
-        #         self.screen,
-        #         colors.black,
-        #         (self.pos[0] + i * self.sqr_size, self.pos[1]),
-        #         (self.pos[0] + i * self.sqr_size, self.pos[1] + 8 * self.sqr_size),
-        #         width=self.width
-        #     )
-            
-        # # draw horizental lines
-        # for i in range(9):
-        #     pygame.draw.line(
-        #         self.screen, 
-        #         colors.black, 
-        #         (self.pos[0], self.pos[1] + i * self.sqr_size), 
-        #         (self.pos[0] + 8 * self.sqr_size, self.pos[1] + i * self.sqr_size), 
-        #         width=self.width
-        #     )
