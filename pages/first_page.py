3d1d3cd037df46dbc103544e31f06a64894a7425
omidziveh <omidziveh1386@gmail.com>
2021-04-10 22:50:18 +0430

    pychess

--- pages/first_page.py
+++ pages/first_page.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.locals import *
 import sys
 import os
 
@@ -9,14 +10,26 @@ from widgets import size
 from widgets import image
 
 
+_image_library = {}
+def get_image(path):
+        global _image_library
+        image = _image_library.get(path)
+        if image == None:
+                canonicalized_path = path.replace('/', os.sep).replace('\\', os.sep)
+                print(canonicalized_path)
+                image = pygame.image.load(canonicalized_path)
+                _image_library[path] = image
+        return image
+
 def page(screen):
 
     start_button_size = size.Size(450, 500, width=100, height=50)
     start_button = button.Button(screen, start_button_size, "play", border=13)
 
-    # logo_size = size.Size(450, 100, 200, 100)
-    # logo = image.Image(screen, 'logo.png', logo_size)
-    # image = pygame.image.load('')
+    logo_size = size.Size(450, 250, 550, 550)
+    logo = image.Image(screen, 'assets\logo.png', logo_size)
+    # image = pygame.image.load(os.path.join('pages', 'logo.png'))
+    # print(os.path.join('pages', 'logo.png'))
     # image = pygame.transform.scale(image, (400, 400))
 
     while True:
@@ -30,7 +43,8 @@ def page(screen):
                 second_page.page(screen)
 
         start_button.draw()
-        # logo.draw()
+        logo.draw()
+        # screen.blit(image, (0, 0))
         # screen.blit(image, (0, 0))
 
         pygame.display.update()
--- widgets/image.py
+++ widgets/image.py
@@ -10,9 +10,24 @@ class Image:
         self.path = path
         self.size = size
 
-        self.image = pygame.image.load('logo.png')
+        self.image = pygame.image.load(path)
 
-        pygame.transform.scale(self.image, (size.width, size.height))
+        self.image = pygame.transform.scale(self.image, (size.width, size.height))
 
     def draw(self):
         self.screen.blit(self.image, (self.size.x, self.size.y))
+    
+    def onTap(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if self.size.rect.collidepoint(event.pos):
+                self.size.rect = (self.size.x, 
+                                  self.size.y + 3, 
+                                  self.width, 
+                                  self.height)
+        if event.type == pygame.MOUSEBUTTONUP:
+            self.size.rect = (self.size.x, 
+                              self.size.y, 
+                              self.size.width, 
+                              self.size.height)
+            if self.size.rect.collidepoint(event.pos):
+                return True
