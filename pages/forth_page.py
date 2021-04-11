8148e50d0fc23f2b3dc37b8c77755af3ce591ef5
omidziveh <omidziveh1386@gmail.com>
2021-04-11 20:14:47 +0430

    pychess

--- pages/forth_page.py
+++ pages/forth_page.py
@@ -7,15 +7,22 @@ from widgets import board
 from widgets import size
 from widgets import menu
 from widgets import datas
+from widgets import button
+from pages import second_page
 
 pygame.init()
 
 def page(screen, users):
+    
     table_size = size.Size(230, 300, 360, 360)
     table = board.Board(screen, table_size, 'w', 45)
     data_table_size = size.Size(700, 300, width=300, height=400)
     data_table = datas.Datas(screen, (100, 100), data_table_size)
-    my_menu = menu.Menu(screen, [])
+    quit_button_size = size.Size(450, 270, width=250, height=50)
+    quit_button = button.Button(screen, quit_button_size, 'QUIT')
+    rematch_button_size = size.Size(450, 350, width=250, height=50)
+    rematch_button = button.Button(screen, rematch_button_size, 'REMATCH')
+    my_menu = menu.Menu(screen, [quit_button, rematch_button])
     
     while True:
         screen.fill(colors.white)
@@ -24,14 +31,24 @@ def page(screen, users):
             if event.type == pygame.QUIT:
                 sys.exit()
             if not my_menu.enabled:
-                pass
-            if data_table.menu_button_tapped(event):
-                my_menu.enabled = True
+                if data_table.menu_button_tapped(event):
+                    my_menu.enabled = True
+            if my_menu.enabled:
+                
+                if my_menu.onTap_close_button(event): # close button
+                    my_menu.enabled = False
+                
+                if my_menu.onTap_button(event, 0): # QUIT button
+                    sys.exit()
+                
+                if my_menu.onTap_button(event, 1): # REAMTCH button
+                    second_page.page(screen)
         
         table.draw()
         data_table.draw()
         
         if my_menu.enabled:
-            my_menu.draw()
+            my_menu.draw(event)
         
         pygame.display.update()
+        pygame.time.Clock().tick(60)
--- widgets/menu.py
+++ widgets/menu.py
@@ -1,16 +1,22 @@
 import pygame
 
+from data import colors
 from widgets import size
+from widgets import text
+from widgets import image
 
 pygame.init()
 
 
 class Menu:
-    def __init__(self, screen, buttons, name="menu", enabled=False, width=300, height=300):
+    def __init__(self, screen, buttons, name="MENU", enabled=False, width=300, height=300):
         self.screen = screen
         self.buttons = buttons
         self.name = name
         self.enabled = enabled
+        self.name_text = self.init_name()
+        self.close_button = self.init_close_button()
+        
         self.display_size = {
             'width': pygame.display.get_surface().get_width(), 
             'height': pygame.display.get_surface().get_height()
@@ -22,8 +28,13 @@ class Menu:
             height=height
         )
         
-    def draw(self):
-        self.disconnect_display()
+    def draw(self, event):
+        if self.enabled:
+            self.disconnect_display()
+            self.draw_self()
+            self.button(event)
+            self.name_text.draw()
+            self.close_button.draw()
         
     
     def disconnect_display(self):
@@ -36,3 +47,37 @@ class Menu:
             rect_surface.get_rect()
         )
         self.enabled = True
+    
+    def connect_display(self):
+        self.enabled = False
+
+    def draw_self(self):
+        pygame.draw.rect(
+            self.screen, 
+            colors.white, 
+            self.size.rect, 
+            border_radius = 20
+        )
+    
+    def button(self, event):
+        for i in range(len(self.buttons)):
+            self.buttons[i].draw()
+            self.buttons[i].onHover(event)
+            
+    def onTap_button(self, event, index):
+        return self.buttons[index].onTap(event)
+    
+    def init_name(self):
+        name_text = text.Text(self.screen, self.name, 375, 190, 45, fg_color=colors.gray_dark)
+        return name_text
+        
+    def init_close_button(self):
+        close_button_size = size.Size(570, 190, width=40, height=40)
+        close_button = image.Image(self.screen, 'assets/close.png', close_button_size)
+        return close_button
+        
+    def draw_close_button(self):
+        self.close_button.draw()
+        
+    def onTap_close_button(self, event):
+        return self.close_button.onTap(event)
